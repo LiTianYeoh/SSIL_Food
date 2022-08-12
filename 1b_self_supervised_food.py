51d046f7c686449bb6e440e370d91136f04cfbf3
LiTianYeoh <yeohlitian@gmail.com>
2022-08-13 02:01:15 +0800

    Update 1ab script for uec

--- 1b_self_supervised_food.py
+++ 1b_self_supervised_food.py
@@ -2,15 +2,19 @@ import torch
 from torchvision.datasets import Food101
 import os
 
-from utils.data_utils import get_f1_ord_dloader, get_f1_off_relic_dloader, get_f1_eval_train_dloader
+from utils.data_utils import \
+    get_f1_ord_dloader, get_f1_off_relic_dloader, get_f1_eval_train_dloader, \
+    get_uec_dloader, get_uec_off_relic_dloader, get_uec_eval_train_loader
+
 from models.offline_models import ReLIC_off
 
 main_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(main_dir,'data')
 f1_root_dir = os.path.join(data_dir, 'food-101')
-vf_root_dir = os.path.join(data_dir, 'vf172')
+uec_root_dir = os.path.join(data_dir, 'uec256')
 
 ## parameter
+ds = 'f1'
 #state_path = None
 state_path = 'relic_offline_e100.pt'
 
@@ -35,27 +39,39 @@ else:
 
 
 # Prepare data
-print('Preparing food101 dataset...')
+if ds == 'f1':
+    print('Preparing food101 dataset...')
 
-data_dir = os.path.join(main_dir,'data')
+    data_dir = os.path.join(main_dir,'data')
 
-print('Checking dataset...')
+    print('Checking dataset...')
 
-check_f101 = Food101(root=data_dir, split='train', download=True)
+    if os.path.exists(f1_root_dir):
 
-print('Obtaining dataloader for supervised offline learning...')
-f1_off_train_loader, f1_off_test_loader, num_off_class = get_f1_ord_dloader(f1_root_dir=f1_root_dir, off_inc = "off", batch_size=batch_s)
-f1_off_relic_train_loader = get_f1_off_relic_dloader(f1_root_dir, batch_size=batch_s)
+        print('Obtaining dataloader for supervised offline learning...')
+        _, f1_off_test_loader, num_off_class = get_f1_ord_dloader(f1_root_dir=f1_root_dir, off_inc = "off", batch_size=batch_s)
+        f1_off_relic_train_loader = get_f1_off_relic_dloader(f1_root_dir, batch_size=batch_s)
 
+        relic_train_loader = f1_off_relic_train_loader
+        test_loader = f1_off_test_loader
 
-relic_train_loader = f1_off_relic_train_loader
-train_loader = f1_off_train_loader
-test_loader = f1_off_test_loader
+    else:
+        print('Could not locate food101 data!')
 
+elif ds == 'uec':
+    print('Preparing uec dataset...')
+
+    print('Checking dataset...')
+    if os.path.exists(uec_root_dir):
+        print('Obtaining dataloader for supervised offline learning...')
+        relic_train_loader = get_uec_off_relic_dloader(uec_root_dir=uec_root_dir, batch_size=batch_s)
+        test_loader, num_off_class = get_uec_dloader(uec_root_dir=uec_root_dir, ltype='off', split ='test', batch_size=batch_s)
+    else:
+        print('Could not locate uec256 data!')
 
 
 # Model and training
-output_dir = os.path.join(main_dir, 'output_model')
+output_dir = os.path.join(main_dir, 'output_model', ds)
 model = ReLIC_off(relic_train_loader, max_epoch, wu_epoch, lr, device, output_dir)
 
 if state_path is not None:
@@ -69,7 +85,11 @@ if to_train:
 
 ### Evaluate performance on test set
 if eval_perf:
-    eval_train_loader, _ = get_f1_eval_train_dloader(f1_root_dir=f1_root_dir, batch_size=batch_s)
+    if ds == 'f1':
+        eval_train_loader = get_f1_eval_train_dloader(f1_root_dir=f1_root_dir, batch_size=batch_s)
+    elif ds == 'uec':
+        eval_train_loader = get_uec_eval_train_loader(uec_root_dir=uec_root_dir, batch_size=batch_s)
+    
     print('Evaluating performance on train set...')
     model.linear_eval_perf(eval_train_loader, num_off_class)
     print('Evaluating performance on test set...')
